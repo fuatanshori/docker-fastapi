d7a7fdc4834d4de250a33036fdceec3c1b596e71
FUAT ANSHORI <anshorifuat12@gmail.com>
2024-05-22 19:47:13 +0700

    all

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI,WebSocket
 app = FastAPI()
 
 @app.get("/")
@@ -10,3 +10,10 @@ def home():
 @app.get("/{name}")
 def helloName(name):
     return {"message": f"hello {name}"}
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        data = await websocket.receive_text()
+        await websocket.send_text(f"Message text was: {data}")
