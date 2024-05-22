d837570445245dc323e12f457d54fd1bcadfc282
FUAT ANSHORI <anshorifuat12@gmail.com>
2024-05-22 17:42:05 +0700

    all

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from typing import Union
 from fastapi import FastAPI
 app = FastAPI()
 
+@app.get("/")
+def home():
+    return {"message":"hello world"}
+
 @app.get("/{name}")
 def helloName(name):
     return {"message": f"hello {name}"}
